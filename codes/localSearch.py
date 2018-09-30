e0a3aacccaa222c1fa23207cf13a197c98c44e3a
yhz012 <603907639@qq.com>
2018-10-01 03:21:07 +0800

    new feature: neighbor

--- codes/localSearch.py
+++ codes/localSearch.py
@@ -2,10 +2,9 @@ import numpy as np
 
 import frame
 from bdastar import biDirectionalAStar as BDAStar, euclideanDist, manhattanDist, chebyshevDist
-from test import mazeFactory as mFty
+import test
 
 class objectiveFunction(object):
-	"""docstring for objectiveFunction"""
 	def __init__(self, w = [1, 0, 0], solutionFunction = BDAStar, solutionConfig = {'LIFO': True, 'distFunction' : manhattanDist}, deRandom = False):
 		#list w with len(w) = 3: weight of blockCount, pathLength, fringeSize
 		#function solutionFunction: algorithms to be tested
@@ -45,8 +44,12 @@ class objectiveFunction(object):
 			return evaluate(maze, self.w, self.solutionFunction, self.solutionConfig, self.deRandom)
 
 class neighbor(object):
-	"""docstring for neighbor"""
 	def __init__(self, size = 4, mutationP = 0.001, mutationFunction = None, mutationConfig = None):
+		#int size in [1 : inf]: the number of child mazes of a singal maze
+		#float mutationP in [0, 1]: probablity of mutation
+		#function mutationFunction: algorithm to generate neighbor maze
+		#dict mutationConifg: configuration of mutationFunction
+		
 		self.size = size
 		self.mutationP = mutationP
 		self.mutationFunction = mutationFunction
@@ -59,21 +62,21 @@ class neighbor(object):
 		def mutation(m, mutationP, mutationFunction, mutationConfig, validate):
 
 			def trivalMutation(m, mutationP, validate):
-				mutationMatrix = np.random.rand(m.shape) < mutationP
+				mutationMatrix = np.random.rand(m.wall.shape[0], m.wall.shape[1]) < mutationP
 				return m.wall ^ mutationMatrix
 
 			if isinstance(m, frame.maze):
 				newMazeList = []
 				count = 0
-				while count < size:
+				while count < self.size:
 					if mutationFunction is None:
-						wall = trivalMutation(m, mutationP)
+						wall = trivalMutation(m, mutationP, validate)
 					else:
 						#TODO: other mutationFunction
 						pass
-					newMaze = frame.maze(m.rows, m.cols, m.p, m.rootNumber)
-					newMaze.build(initFunction = frame.maze.build.setWall, initConfig = {'wall': wall})
-					if validate and not mFty.valid(newMaze):
+					newMaze = frame.maze(m.rows, m.cols, m.p, m.rootNum)
+					newMaze.build(initFunction = frame.setWall, initConifg = {'wall': wall})
+					if validate and not test.valid(newMaze):
 						continue
 					newMazeList.append(newMaze)
 					count = count + 1
@@ -85,22 +88,27 @@ class neighbor(object):
 		if isinstance(maze, list) or isinstance(maze, tuple):
 			neighborList = []
 			for m in maze:
-				resultList.extend(mutation(m, self.mutationP, self.mutationFunction, self.mutationConfig, self.validate))
+				neighborList.extend(mutation(m, self.mutationP, self.mutationFunction, self.mutationConfig, validate))
 			return neighborList
 		else:
-			return mutation(m, self.mutationP, self.mutationFunction, self.mutationConfig, self.validate))
+			return mutation(maze, self.mutationP, self.mutationFunction, self.mutationConfig, validate)
 		
 
-def beamSearch(mList, teleportLimit = 0, maxIteration = 100, temperature, ):
+def beamSearch(mList, teleportLimit = 0, maxIteration = 100, temperature = 0):
 	pass
 
 if __name__ == '__main__':
-	a = frame.maze()
+	a = frame.maze(rootNum = 1)
 	a.build()
-	b = frame.maze()
+	b = frame.maze(rootNum = 2)
 	b.build()
 	sf = BDAStar
 	sc = {'LIFO': True, 'distFunction' : manhattanDist}
+	nb = neighbor(size = 2, mutationP = 0.02)
+	newMaze = nb([a,b], validate = True)
 	of = objectiveFunction([1,1,1], sf, sc, deRandom = 2)
-	print(of([a,b]))
+	print(newMaze)
+	for m in newMaze:
+		print(m.rootNum)
+	print(of(newMaze))
 	print(BDAStar(a, **sc))
